2f5279f8ab09a2d396be04f93551437b1c07cb73
camilo3900 <camilogv.1629@gmail.com>
2025-02-17 05:52:21 -0500

    subiendo el proyecto inicial

---
+++ src/main/java/com/gualteros/weaponsStore/WeaponsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.gualteros.weaponsStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaponsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaponsStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gualteros/weaponsStore/WeaponsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gualteros.weaponsStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaponsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
