5106bf2000ad67f09ad85541214295591bd038df
camilo3900 <camilogv.1629@gmail.com>
2025-02-17 18:14:39 -0500

    se crea ProductoDto para mapear las entidades de la DB en la aplicacion

---
+++ src/main/java/com/gualteros/weaponsStore/models/dto/ProductoDto.java
@@ -0,0 +1,14 @@
+package com.gualteros.weaponsStore.models.dto;
+
+import com.gualteros.weaponsStore.models.Categoria;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProductoDto {
+    private String nombreDto;
+    private Categoria categoriaDto;
+    private Double precioDto;
+}
