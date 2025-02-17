f7ac281d96f6be1823b494a9997d916cab0f15a9
camilo3900 <camilogv.1629@gmail.com>
2025-02-17 18:45:33 -0500

    nuevas consultas Comprador para busquedas avanzadas en la DB

--- src/main/java/com/gualteros/weaponsStore/repository/CompradorRepository.java
+++ src/main/java/com/gualteros/weaponsStore/repository/CompradorRepository.java
@@ -1,11 +1,18 @@
 package com.gualteros.weaponsStore.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.gualteros.weaponsStore.models.Comprador;
+import com.gualteros.weaponsStore.models.dto.CompradorDto;
 
 @Repository
 public interface CompradorRepository extends JpaRepository<Comprador, Long> {
 
+    @Query("SELECT * FROM Comprador WHERE apellido = : apellidoParam")
+    List<CompradorDto> getCompradorByApellido(@Param("apellidoParam") String apellido);
 }
