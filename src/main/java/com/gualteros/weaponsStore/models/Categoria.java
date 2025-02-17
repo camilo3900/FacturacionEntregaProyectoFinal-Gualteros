fb0a0bf630ce333deaccf048c82997c3776735a1
camilo3900 <camilogv.1629@gmail.com>
2025-02-17 18:30:47 -0500

    SE AGREGA ATRIBUTO STOCK EN PRODUCTO Y CATEGORIA

---
+++ src/main/java/com/gualteros/weaponsStore/models/Categoria.java
@@ -0,0 +1,9 @@
+package com.gualteros.weaponsStore.models;
+
+public enum Categoria {
+    RIFLES,
+    PISTOLAS,
+    EQUIPOS,
+    MUNICIONES,
+    DAGAS,
+}
--- src/main/java/com/gualteros/weaponsStore/models/Producto.java
+++ src/main/java/com/gualteros/weaponsStore/models/Producto.java
@@ -1,5 +1,7 @@
 package com.gualteros.weaponsStore.models;
 
+import com.gualteros.weaponsStore.models.dto.ProductoDto;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
 @Entity
 @Table(name = "productos")
 @Data
@@ -21,7 +24,15 @@ public class Producto {
     private Long id;
     @Column(name = "nombre_producto")
     private String nombre;
+    @Column(name = "categoria")
+    private Categoria categoria;
     @Column(name = "precio")
     private Double precio;
+    @Column(name = "stock")
+    private Integer stock;
+
+    public ProductoDto toProductoDto() {
+        return new ProductoDto(this.nombre, this.categoria, this.precio);
+    }
 
 }
